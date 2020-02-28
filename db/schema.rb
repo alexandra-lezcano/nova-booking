059ca05f9347e162c45cc2991d2d6e85b6435d3c
enzoM97 <enzomarecos@gmail.com>
2020-02-27 23:15:25 -0300

    first commit dev-em

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_211345) do
+ActiveRecord::Schema.define(version: 2020_02_27_004852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
@@ -44,16 +65,62 @@ ActiveRecord::Schema.define(version: 2020_02_22_211345) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "facilities_estates", id: false, force: :cascade do |t|
+    t.bigint "facility_id"
+    t.bigint "estate_id"
+    t.integer "quantity"
+    t.index ["estate_id"], name: "index_facilities_estates_on_estate_id"
+    t.index ["facility_id"], name: "index_facilities_estates_on_facility_id"
+  end
+
+  create_table "facilities_rooms", id: false, force: :cascade do |t|
+    t.bigint "facility_id"
+    t.bigint "room_id"
+    t.integer "quantity"
+    t.index ["facility_id"], name: "index_facilities_rooms_on_facility_id"
+    t.index ["room_id"], name: "index_facilities_rooms_on_room_id"
+  end
+
+  create_table "owners", force: :cascade do |t|
+    t.string "phone"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "about"
+    t.string "email"
+    t.string "name"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.string "description"
     t.string "capacity"
     t.integer "price"
-    t.integer "status"
+    t.string "status"
     t.string "room_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "estate_id", null: false
+    t.index ["estate_id"], name: "index_rooms_on_estate_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "last_name"
+    t.string "address"
+    t.string "phone"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cities", "departaments"
   add_foreign_key "estates", "cities"
+  add_foreign_key "rooms", "estates"
 end
