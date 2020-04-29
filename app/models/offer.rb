f883c2ff8e89592d308f730dec843b282386d7ba
Adrian Holman <adrian.holman@fiuni.edu.py>
2020-04-29 01:39:15 -0400

    Update offer.rb

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,4 +3,16 @@ class Offer < ApplicationRecord
   has_many :offer_details, dependent: :destroy
   accepts_nested_attributes_for :offer_details, allow_destroy: true,
                                 reject_if: :all_blank
+  delegate :name, :to => :estate, :prefix => true
+  delegate :images, :to => :estate, :prefix => true
+  self.per_page = 5
+
+  scope :offers_by_owner, -> (current_owner_id) {
+    joins(:estate).where('estates.owner_id = ?',current_owner_id)
+  }
+
+  def is_available_for?(date_start, date_end)
+    (self.date_start..self.date_end).cover?(date_start) or
+        (self.date_start..self.date_end).cover?(date_end)
+  end
 end
